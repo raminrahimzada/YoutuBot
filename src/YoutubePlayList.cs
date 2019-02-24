aff7f89ce3d918664039a111c57bed5725db2d8e
Ramin Rahimzada <raminrahimzada@gmail.com>
2019-02-24 11:58:11 -0800

    Create YoutubePlayList.cs

---
+++ src/YoutubePlayList.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace YoutuBot
+{
+    public class YoutubePlayList
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AuthorName { get; set; }
+        public string Views { get; set; }
+        public List<YoutubeVideoInfo> Videos { get; set; }
+        public string Id { get; set; }
+        public string Thumbnail { get; set; }
+        public string VideoCount { get; set; }
+        public string PublishedTime { get; set; }
+        public string[] SidebarThumbnails { get; set; }
+    }
+}
